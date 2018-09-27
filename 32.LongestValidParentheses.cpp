1aa2b55669e22c4d7fad687d9e74a596c1ac9516
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2018-09-27 14:57:24 +0800

    Improve CMakefile

--- 32.LongestValidParentheses.cpp
+++ 32.LongestValidParentheses.cpp
@@ -4,22 +4,30 @@
 #include <string>
 
 class Solution {
-public:
-    int longestValidParentheses(std::string s) {
-        std::stack<char> stack;
-        std::size_t curr_len = 0, max_len = 0;
-        for(auto it = s.begin(); it != s.end(); ++it) {
-            if(stack.size() == 0) {
-                curr_len = 0;
-            } else if(stack.top() == '(' && *it == ')') {
-                curr_len ++;
-            }
-            
-            if(curr_len >= max_len) {
-                max_len = curr_len;
-            }
-        }
-    }
+  public:
+	int longestValidParentheses(std::string s) {
+	  std::stack<char> stack;
+	  std::size_t curr_len = 0, max_len = 0;
+	  for(auto it = s.begin(); it != s.end(); ++it) {
+		if(*it == '(') {
+		  stack.push(*it);
+		}
+
+		if(*it == ')') {
+		  if(stack.size() == 0) {
+			curr_len = 0;
+		  } else if(stack.top() == '(') {
+			stack.pop();
+			curr_len ++;
+		  }
+		}
+
+		if(curr_len >= max_len) {
+		  max_len = curr_len;
+		}
+	  }
+	  return max_len * 2;
+	}
 };
 
 
@@ -27,7 +35,7 @@ int main() {
   Solution sol;
 
   std::vector<const char *> input = {
-	"(()", ")()())"
+	"(()", ")()())", "()(()"
   };
 
   for(auto it = input.begin(); it != input.end(); ++it) {
